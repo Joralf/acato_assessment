537eb28a197a16a267d709dfcbfc74a58aea83f3
Joralf Quist <j.quist@meteovista.nl>
2017-04-23 13:41:02 +0200

    add profile picture avatar plus some generic styling

--- source/components/common/employeeItem.jsx
+++ source/components/common/employeeItem.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ProfilePicture from '../../../assets/profile_picture.png';
+
 import './employeeItem.scss';
 
 const EmployeeItem = ({ item, color }) => (
   <div className="card" style={{'backgroundColor': color}}>
-    <h3>{item.name}</h3>
-    <div>Role: {item.role}</div>
-    <div>Quote: {item.quote}</div>
+    <img className="avatar" src={ProfilePicture} alt="profile_picture" />
+    <div className="content">
+      <h4>{item.name}</h4>
+      <div>Role: {item.role}</div>
+      <div>Quote: {item.quote}</div>
+    </div>
   </div>
 );
 
